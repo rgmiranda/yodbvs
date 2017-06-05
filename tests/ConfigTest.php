5ccc0d853b79c42a204702080f98c6c2b4496044
Ricardo Miranda <rmiranda@jussantiago.gov.ar>
2017-06-05 08:51:38 -0300

    Config tests

--- tests/ConfigTest.php
+++ tests/ConfigTest.php
@@ -3,8 +3,25 @@
 namespace yodbvs;
 
 class ConfigTest extends \PHPUnit_Framework_TestCase {
-    public function testGet() {
+    public function testGetScalar() {
         $cfg = Config::getInstance();
         $this->assertEquals('MySQL', $cfg->get('db.adapter'));
+    }
+
+    public function testGetArray() {
+        $cfg = Config::getInstance();
+        $this->assertEquals(array (
+            'adapter'   => 'MySQL',
+            'host'      => 'localhost',
+            'user'      => 'root',
+            'pass'      => 'sacha666',
+            'dbname'    => 'sigepe',
+            'port'      => '3306',
+        ), $cfg->get('db'));
+    }
+
+    public function testGetNull() {
+        $cfg = Config::getInstance();
+        $this->assertNull($cfg->get('none'));
     }
 }
