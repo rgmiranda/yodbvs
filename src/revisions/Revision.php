49778e1efc6f77e68a182d2ca05de657def9db79
Ricardo Miranda <rmiranda@jussantiago.gov.ar>
2017-06-06 10:34:39 -0300

    Clase para la ejecución de revisiones.

---
+++ src/revisions/Revision.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace yodbvs\revisions;
+
+class Revision {
+    
+}
---
+++ tests/revisions/RevisionTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace yodbvs\revisions;
+use \yodbvs\Config;
+
+class RevisionTest extends \PHPUnit_Framework_TestCase {
+    protected function setUp() {
+        $this->filename = Config::getInstance()->get('misc.datafile');
+        if (file_exists($this->filename)) {
+            $this->tmpfilename = tempnam(sys_get_temp_dir(), 'yodbvs-');
+            file_put_contents($this->tmpfilename, file_get_contents($this->filename));
+            unlink($this->filename);
+        }
+    }
+
+    protected function tearDown() {
+        if (!empty($this->tmpfilename)) {
+            file_put_contents($this->filename, file_get_contents($this->tmpfilename));
+            unlink($this->tmpfilename);
+        }
+    }
+
+    public function testGetNull() {
+        $curr = CurrentRevision::getInstance();
+        if (file_exists($this->filename)) {
+            unlink($this->filename);
+        }
+        $this->assertNull($curr->get());
+    }
+
+    public function testGet() {
+        $curr = CurrentRevision::getInstance();
+        file_put_contents($this->filename, '46');
+        $this->assertEquals(46, $curr->get());
+    }
+
+    public function testSet() {
+        $curr = CurrentRevision::getInstance();
+        $curr->set(789);
+        $this->assertEquals(789, $curr->get());
+    }
+
+    /**
+     * @expectedException InvalidArgumentException
+     */
+    public function testSetNegative() {
+        $curr = CurrentRevision::getInstance();
+        $curr->set(-789);
+    }
+}
